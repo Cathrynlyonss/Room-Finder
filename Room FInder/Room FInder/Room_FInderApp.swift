d99ff43f41ff86def705a0b3ca28aeba254966c6
Cathryn Lyons <cathryn-lyons@uiowa.edu>
2023-01-23 12:52:19 -0600

    blank app

---
+++ Room FInder/Room FInder/Room_FInderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Room_FInderApp.swift
+//  Room FInder
+//
+//  Created by Cathryn Lyons on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Room_FInderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
